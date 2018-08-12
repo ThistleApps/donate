93fc41cb84657dec7c523d6e8bcf4f43a1084b5f
thistleapps <mduncan77@comcast.net>
2018-08-12 12:33:03 -0400

    first commit

---
+++ app/Church.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Church extends Model
+{
+    //
+    public $timestamps = false;
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+
+                        </div>
+
+                    @endif
+
+                        <div><h3>You are logged in and will see your dashboard and activity</h3></div>
+{{--                        <div class="container">
+                            <br><br>
+                            <div class="row">
+                                @foreach($posts as $post)
+                                    <div class="col-md-3">
+                                        <a href="/post/{{ $post->slug }}">
+                                            <img src="{{ Voyager::image( $post->image ) }}" style="width:100%">
+                                            <span>{{ $post->title }}</span>
+                                        </a>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>--}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
